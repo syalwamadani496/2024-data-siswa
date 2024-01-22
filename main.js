1afcea1a037a0094d38544d7601a4c7fb9774257
syalwamadaniscout@gmail.com <syalwamadaniscout@gmail.com>
2024-01-22 11:13:49 +0700

    menambah konfigurasi ferbes slebew

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { 
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbLsq27KJU9tD9pHC8GrPUB7LgEPEQbPU",
+  authDomain: "insan-cemerlang-92ee0.firebaseapp.com",
+  projectId: "insan-cemerlang-92ee0",
+  storageBucket: "insan-cemerlang-92ee0.appspot.com",
+  messagingSenderId: "332441427242",
+  appId: "1:332441427242:web:73c31309147ef1dab15253",
+  measurementId: "G-JW04DZL85R"
+};
+
+const app = initializeApp(firebaseConfig);
